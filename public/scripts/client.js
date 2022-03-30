509a94b38589e80849873b6d8ed5f2a3f4b334b2
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-30 13:57:00 -0400

    add jquery to go through tweets and post all to page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = (tweetData) => {
+  let tweet = `
+  <article class="tweet">
+        <header class="tweets-header">
+          <div class="name">
+            <img src="${tweetData.user.avatars}">
+            ${tweetData.user.name}
+          </div>
+          <div class="username">
+            ${tweetData.user.handle}
+          </div>
+        </header>
+        <p class="text">
+        ${tweetData.content.text}</p>
+        <footer class="tweets-footer">
+          ${tweetData.created_at}
+          <div>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>`
+  $('.tweets-container').append(tweet);
+};
+
+const renderTweets = (data) => {
+  for (let tweets of data){
+    createTweetElement(tweets)
+  }
+};
+
+
+$(() => {
+  $('.tweets-container').on('submit', (event) => {
+    event.preventDefault();
+  });
+  
+  
+  renderTweets(data);
+});
